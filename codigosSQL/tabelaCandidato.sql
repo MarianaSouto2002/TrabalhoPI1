0526ff019246019251ca4ae2d17a91768accf420
matheus <matheushcp7@ufu.br>
2024-11-10 12:40:05 -0300

    aaaaaaaaaaaaaaaaaaa

--- codigosSQL/tabelaCandidato.sql
+++ codigosSQL/tabelaCandidato.sql
@@ -1,6 +1,6 @@
-CREATE TABLE candidatos (
+CREATE TABLE candidato (
     nome VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) PRIMARY KEY,
+    cpf VARCHAR(14) PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
     data_nasc DATE NOT NULL,
--- codigosSQL/tabelaVagaCandidato.sql
+++ codigosSQL/tabelaVagaCandidato.sql
@@ -8,5 +8,5 @@ CREATE TABLE vaga_candidato (
     CONSTRAINT fk_vaga_candidato_cnpj FOREIGN KEY (cnpj)
         REFERENCES empresa(cnpj) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_vaga_candidato_id_post FOREIGN KEY (id_post)
-        REFERENCES post(id_post) ON DELETE CASCADE ON UPDATE CASCADE
+        REFERENCES post(id_post) ON DELETE CASCADE ON UPDATE CASCADE
 );
